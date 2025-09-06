fd22082e204668b2fa19171e2f525c3942318662
frankamdev <fkamgang06@gmail.com>
2025-09-06 01:39:48 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard', [
+        'usersPosts' => Auth::user()->posts()->with('author')->latest()->get()
+    ]);
+});
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::post('/posts/{post./like}', [PostController::class, 'like'])->name('posts.like');
 });
+
+
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
